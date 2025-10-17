a149772513a98d5720de3f36f2025a106853dc87
Gaurav Shukla <gaurav639147@gmail.com>
2025-10-18 00:07:02 +0530

    updating the file pattern.js

--- pattern.js
+++ pattern.js
@@ -1,14 +1,13 @@
+// 
+
 let prompt = require('prompt-sync')();
 let n = Number(prompt("Enter the number: "));
 
 for (let i = 1; i <= n; i++) {
   // left spaces
+  process.stdout.write("  ");
   for (let j = 1; j <= n; j++) {
     process.stdout.write(" * ");
   }
-  // stars
-  for (let j = 1; j <= n- 1; j++) {
-    process.stdout.write(" ");
-  }
-  console.log();
+  
 }
