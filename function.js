3cb869806ee3a5380074cfd89ee832c3f2fe3a42
Gaurav Shukla <gaurav639147@gmail.com>
2025-10-28 11:25:46 +0530

    add the new file

---
+++ function.js
@@ -0,0 +1,17 @@
+//normal function 
+
+function sayhello(){
+    console.log("Hello world  ")
+}
+
+return sayhello() // call , run , invoke 
+
+// how to call 
+function sayhellos(name = "user"){
+    console.log("Hello " + name)
+}
+
+ sayhellos("gauravs")
+ sayhellos("saurabh") 
+ sayhellos("sarvgya") 
+ sayhellos() // call , run , invoke 
