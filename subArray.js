a7596b45696695e8ac4d806aa7f3892de924f9d8
Gaurav Shukla <gaurav639147@gmail.com>
2025-11-07 18:50:15 +0530

    updating the file

---
+++ subArray.js
@@ -0,0 +1,8 @@
+let arr = [ 1, 2,3 ,4 ,5]
+for(let i = 0 ; i< arr.length ; i++){
+    let sub = []
+    for(let j = i ; j < arr.length; j++){
+        sub += arr[j]
+        console.log(sub)
+    }
+}
