8391c3ca65dc49e4649189b4a05b20a70cdf1871
Gaurav Shukla <gaurav639147@gmail.com>
2025-11-12 22:24:53 +0530

    updating the file.js

--- subArray.js
+++ subArray.js
@@ -6,3 +6,43 @@ for(let i = 0 ; i< arr.length ; i++){
         console.log(sub)
     }
 }
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let arr1 = [1,2,3,4,5,6,7,8,9,10];
+
+rl.on('line', (input) => {
+  let [n, s] = input.split(' ').map(Number);
+  solve(arr, n, s);
+  rl.close();
+});
+
+function solve(arr, n, s) {
+  // Write your code here
+  let count = 0 ;
+  for(let i = 0 ; i< n ; i++){
+       count += arr[i]
+         
+  }
+  if(count ===s){
+    console.log("YES")
+      return
+  }
+
+   for (let i = n; i < arr.length; i++) {
+    count += arr[i] - arr[i - n]; // add next, remove first
+    if (count === s) {
+      console.log("YES");
+      return;
+    }
+  }
+
+  console.log("NO");
+
+  
+}
