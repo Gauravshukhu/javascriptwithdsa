685d3863b3391d49bb361800de3f6a2ed0d6db37
Gaurav Shukla <gaurav639147@gmail.com>
2025-12-10 14:48:29 +0530

    update the function.js file

--- function.js
+++ function.js
@@ -1,57 +1,157 @@
-// These are normal  function 
+// // These are normal  function 
 
-// sum
-function sum(a,b){
-    console.log(a+b)
+// // sum
+// function sum(a,b){
+//     console.log(a+b)
+// }
+// sum(10, 20 )
+
+// // sub
+// function sub(o,l){
+//     console.log(o-l)
+// }
+// sub(20,10)
+
+// // mul
+// function mul(m,n){
+//     console.log(m*n *q)
+// }
+// mul(20,30, 80)
+
+// function div(q , p){
+//     console.log(q/p)
+// }
+// div(20,2)
+
+// function all(c,v){
+//     console.log(c+v);
+//     console.log(c-v);
+//     console.log(c*v);
+//      console.log(c/v)
+
+
+// }
+// all(20,2)
+
+// // these are functional expression 
+
+// const Cars = function(model = "none"){
+//     return "HELLO" + " " +model
+// }
+
+// console.log(Cars("Hyndaii"))
+// console.log(Cars("Toyta"))
+
+// // arrow function
+
+
+// // 1st condition
+// const add = (a,b) =>{
+//     return a+b ;
+// }
+// console.log(add(1,10))
+
+// //2nd condition
+
+// const add1 = (a,b) => a+b
+// console.log(add1(1,10))
+
+
+
+
+function frequencyDigit(n,d){
+    let count = 0 ; 
+    let str = n.toString();
+
+    for(let i = 0 ; i< str.length; i++){
+        if(str[i] === d.toString()){
+            count++
+        }
+    }
+    return count
 }
-sum(10, 20 )
 
-// sub
-function sub(o,l){
-    console.log(o-l)
-}
-sub(20,10)
+console.log(frequencyDigit(986272,2 ))
 
-// mul
-function mul(m,n){
-    console.log(m*n *q)
-}
-mul(20,30, 80)
 
-function div(q , p){
-    console.log(q/p)
-}
-div(20,2)
+function addNumber (a,b){
+    let sum = a+b;
 
-function all(c,v){
-    console.log(c+v);
-    console.log(c-v);
-    console.log(c*v);
-     console.log(c/v)
-
+    return sum
 
 }
-all(20,2)
+console.log(addNumber(10 , 20))
 
-// these are functional expression 
 
-const Cars = function(model = "none"){
-    return "HELLO" + " " +model
+// 3️⃣ Write a function to check if a number is even or odd.
+function checkevenodd(n) {
+ if(n % 2 == 0 ){
+  return "even"
+ }else{
+    return "odd"
+ }
 }
 
-console.log(Cars("Hyndaii"))
-console.log(Cars("Toyta"))
+console.log(checkevenodd(45))
 
-// arrow function
+// 15️⃣ Function to reverse an array.
 
 
-// 1st condition
-const add = (a,b) =>{
-    return a+b ;
+function reversearr(arr){
+    let start = 0 ;
+    let end = arr.length-1;
+
+    while(start<end){
+        let temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp 
+
+        start++
+        end--
+    }
+    return arr 
 }
-console.log(add(1,10))
 
-//2nd condition
+console.log(reversearr([3 , 10 , 9 , 7 , 6]))
 
-const add1 = (a,b) => a+b
-console.log(add1(1,10))
+
+// 19️⃣ Function to sort array in ascending order.
+function sortAscending(arr){
+  
+    for(let i = 0 ; i< arr.length-1 ; i++){
+        let j = i+1;
+        while(j>0 && arr[j]<arr[j-1]){
+            let temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp 
+            j--
+        }
+    }
+    return arr
+}
+
+console.log(sortAscending([15, 19, 13, 14 , 10]))
+
+
+// 20️⃣ Function to sort array in descending order.
+
+function sortDscending(arr){
+  
+    for(let i = 0 ; i< arr.length-1 ; i++){
+        let j = i+1;
+        while(j>0 && arr[j]>arr[j-1]){
+            let temp = arr[j]
+            arr[j] = arr[j-1]
+            arr[j-1] = temp 
+            j--
+        }
+    }
+    return arr
+}
+
+console.log(sortDscending([15, 19, 13, 14 , 10]))
+
+
+
+
+
